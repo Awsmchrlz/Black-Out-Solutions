0e585e886dc77e474f5291c4485dfa554d58ff10
Awsmchrlz <chisalecharles23@gmail.com>
2024-06-27 14:00:43 +0200

    authentication setup,no live databasee

--- utilities/gmailUtils.js
+++ utilities/gmailUtils.js
@@ -4,7 +4,7 @@ var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 var apiKey = defaultClient.authentications["api-key"];
 
-apiKey.apiKey = 	process.env.BREVO_EMAIL_API;
+apiKey.apiKey =	proccess.env.BREVO_EMAIL_API;
 
 var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
