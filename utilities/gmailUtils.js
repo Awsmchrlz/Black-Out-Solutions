9c51ddee5567e2dff4ca7bbfa970126a1d0d742e
Awsmchrlz <chisalecharles23@gmail.com>
2024-06-27 14:25:32 +0200

    Remove Sendinblue API key from utilities/gmailUtils.js

--- utilities/gmailUtils.js
+++ utilities/gmailUtils.js
@@ -1,8 +1,7 @@
 // require("dotenv").config();
 var SibApiV3Sdk = require("sib-api-v3-sdk");
 var defaultClient = SibApiV3Sdk.ApiClient.instance;gi
-var apiKey = defaultClient.authentications["api-key"];
-apiKey.apiKey = process.env.BREVO_EMAIL_API;
+
 
 var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
