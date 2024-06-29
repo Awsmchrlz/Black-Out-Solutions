10f715bb2eca6aff181b191cfb7d1ce6c7d3eea5
Awsmchrlz <chisalecharles23@gmail.com>
2024-06-29 15:30:34 +0200

    auth cleans and more

--- utilities/gmailUtils.js
+++ utilities/gmailUtils.js
@@ -3,8 +3,8 @@ var SibApiV3Sdk = require("sib-api-v3-sdk");
 var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 var apiKey = defaultClient.authentications["api-key"];
-
-apiKey.apiKey = process.env.BREVO_EMAIL_API;
+const key = process.env.BREVO_EMAIL_API;
+apiKey.apiKey = key;
 
 var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
