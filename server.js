a7811a705e34012bfbba962fc1c2a175bfa48017
Awsmchrlz <chisalecharles23@gmail.com>
2024-07-09 07:30:55 +0200

    Search functionality

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const localDb = "mongodb://127.0.0.1:27017/blackout"
 
 // process.env.
 mongoose.set('strictQuery', true);
-mongoose.connect(localDb, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.end.liveDb, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database is connected'))
   .catch(err => console.log('Error connecting to database', err));
 
